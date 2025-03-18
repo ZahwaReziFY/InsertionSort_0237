c71587cbdcdab92f864dcffec06d282dbcc3cb69
ZahwaReziFY <zahwarezi416@gmail.com>
2025-03-18 21:54:43 +0700

    create void function for cin variable

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 int arr[20]; // Membuat array dengan panjang data 20
 int n; // Membuat variable inputan n
+
+void input()
+{ // Procedure Input
+while (true)
+{
+cout << "Masukan JUmlah Data pada Array : "; // Membuat Inputan jumlah element Array
+cin >> n; // memanggil variable inputan n
+
+} }
