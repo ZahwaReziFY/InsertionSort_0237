f6cb78daf8e77f33ad6e1da82996213181b8b1a2
ZahwaReziFY <zahwarezi416@gmail.com>
2025-03-18 22:01:43 +0700

    add if statement

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -8,7 +8,20 @@ void input()
 { // Procedure Input
 while (true)
 {
-cout << "Masukan JUmlah Data pada Array : "; // Membuat Inputan jumlah element Array
+cout << "Masukan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
 cin >> n; // memanggil variable inputan n
 
-} }
+if (n <= 20)
+{ // Membuat kondisi n tidak lebih dari 20
+break;
+}
+else
+{
+cout << "\nArray yang anda masukan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+}
+}
+cout << endl; // Membuat jarak per baris program
+cout << "=====================" << endl; // Membuat tampilan susunan data element array
+cout << "Masukan Element Array" << endl;
+cout << "=====================" << endl;
+}
